f52afc4c4181e6b1fce0dd64538c293391541b05
Tatsuki Isogami <tatsuki5820iso@gmail.com>
2020-02-09 16:36:54 +0900

    create simple web server(localhost:9090) & edited README.md
    fix

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # HelloGo
 test repository for golang
+
+## Quick start
+```
+cd /path/to/HelloGo
+go build main.go
+./main
+```
+
+## Reference
+https://astaxie.gitbooks.io/build-web-application-with-golang/ja/03.2.html
--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import "net/http"
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "strings"
+)
 
 func main() {
-    http.HandleFunc("/", hello)
-    http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", hello) //アクセスのルーティングを設定します。
+    err := http.ListenAndServe(":9090", nil) //監視するポートを設定します。
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    w.Write([] byte("hello!"))
+    _ = r.ParseForm()   //オプションを解析します。デフォルトでは解析しません。
+    fmt.Println(r.Form) //このデータはサーバのプリント情報に出力されます。
+    fmt.Println("path", r.URL.Path)
+    fmt.Println("scheme", r.URL.Scheme)
+    fmt.Println(r.Form["url_long"])
+    for k, v := range r.Form {
+        fmt.Println("key:", k)
+        fmt.Println("val:", strings.Join(v, ""))
+    }
+    fmt.Fprintf(w, "Hello astaxie!") //ここでwに入るものがクライアントに出力されます。
 }
 
